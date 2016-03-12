61ab3dd7cc7746ad8199412fb6f8eef8bb2d5d1e
cnzau <clement.nzau@gmail.com>
2016-03-12 12:00:56 +0300

    instructions remainder finder

--- java/RemainderFinder.java
+++ java/RemainderFinder.java
@@ -16,3 +16,23 @@ class RemainderFinder {
     return rem;
   }
 }
+
+/*
+Task
+
+Write a method remainder which takes two integer arguments, dividend and divisor, and returns the remainder when dividend is divided by divisor. Do NOT use the modulus operator (%) to calculate the remainder!
+
+Assumption
+
+Dividend will always be greater than or equal to divisor.
+
+Notes
+
+Make sure that the implemented remainder function works exactly the same as the Modulus Operator (%). For example n % 0 = NaN, your method should return null.
+
+Some Random Test Cases are waiting up ahead...
+
+To Java warriors...
+
+SimpleInteger is a tiny and immutable implementation of an integer 
+*/
