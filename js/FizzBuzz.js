e765f5cb3187a2a316a886e26ebd14b6ee7f4488
cnzau <clement.nzau@gmail.com>
2016-04-09 22:21:25 +0300

    Fizz Buzz

---
+++ js/FizzBuzz.js
@@ -0,0 +1,35 @@
+// Return an array
+function fizzbuzz(n)
+{
+  var arr = [];
+  if (n >= 1) {
+    for (var i = 1; i <= n; i++) {
+      if (i % 15 === 0) {
+        arr.push('FizzBuzz');
+      } else if (i % 5 === 0) {
+        arr.push('Buzz');
+      } else if (i % 3 === 0) {
+        arr.push('Fizz');
+      } else {
+        arr.push(i);
+      }
+    }
+    return arr;
+  }
+}
+var fizzify = fizzbuzz;
+
+/*
+Fizz Buzz
+
+Description:
+
+Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value 'Fizz' instead
+If the value is a multiple of 5: use the value 'Buzz' instead
+If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+
+*/
